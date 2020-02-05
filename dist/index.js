4ee4b2275523369db7574a46312b1712478a71b6
Michael Hettmer <mail@michael-hettmer.de>
2020-02-05 18:23:54 +0100

    ci: remove build assets in repository

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.entry = () => {
-    console.log('tbd, but entry works');
-    return 0;
-};
-exports.entry();
-//# sourceMappingURL=index.js.map
