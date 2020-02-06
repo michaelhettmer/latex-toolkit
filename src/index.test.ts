0590e342aecd52227d5d28940fcc07f955f63dd3
Michael Hettmer <mail@michael-hettmer.de>
2020-02-06 19:01:43 +0100

    build: add ts importHelpers

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,5 @@
 import mockConsole from 'jest-mock-console';
-import { entry } from './index';
+import { entry } from '.';
 
 describe('test placeholder', () => {
     it('should call console.log once and return 0', () => {
